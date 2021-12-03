a55396f5a1ae23032cea8a712c28bf1f86d0b50a
ak <anishkmr43@gmail.com>
2021-12-03 12:59:49 -0600

    Create Script1.py
    it's a basic script used send_command() method of netmiko.

---
+++ Script1.py
@@ -0,0 +1,30 @@
+#this is a basic script, establishes the ssh connection to the network device and checkwhether the enable mode is configured on not. Then send a command using send_command() method
+
+from netmiko import ConnectHandler
+
+dict123={
+'ip':'192.168.30.151',
+'username':'admin',
+'password':'cisco',
+'device_type':'cisco_ios'
+}
+
+ssh123=ConnectHandler(**dict123)      #thischecksthedevice_typeandestablishestheconnection
+print(ssh123)
+a=ssh123.find_prompt()
+print("sshissuccessfulto"+a)
+input_1='en'
+output_1=ssh123.send_command(input_1)
+print(output_1)
+test=ssh123.check_enable_mode()
+print(test)
+
+#===============================================================================================================
+
+'''
+b=["showipintbr","shclock"]
+for abc in b:
+out123=ssh123.send_command(abc)
+print("FETCHINGTHEOUTPUTFOR"+abc)
+print(out123)
+'''
