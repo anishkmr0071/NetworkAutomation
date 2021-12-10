f34cec19471ecaf924455b7aa70b67b6f3b5498b
Anish Yavapuram <anishkmr43@gmail.com>
2021-12-10 10:08:44 -0600

    Create Script13.py
    # This script will try to ask for credentials until the authentication is successful.

---
+++ Script13.py
@@ -0,0 +1,36 @@
+# This script will try to ask for credentials until the authentication is successful. 
+#used: logging, datetime, try and except (NetmikoAuthenticationException).
+import logging
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetmikoAuthenticationException
+logging.basicConfig(filename="today_1.log", level=logging.DEBUG)
+logger = logging.getLogger("netmiko")
+import datetime
+ip_add = ["192.168.30.171", "192.168.30.172"]
+for i in ip_add:
+    a = 1
+    while a<=5:
+        try:
+            device_info = {
+                'device_type': 'cisco_ios',
+                'ip': i,
+                'username': input("enter the  username: "),
+                'password': input("enter the password: "),
+                'secret': 'cisco'
+            }
+
+            #time.sleep(5)
+            ssh_session = ConnectHandler(**device_info)
+            ssh_session.enable()
+            out123 = ssh_session.find_prompt()
+            print(out123)
+
+            if ">" or "#" in out123:
+                print(" login is successful " + i)
+                break
+            else:
+                pass
+        except NetmikoAuthenticationException:
+            print("trying again")
+        a = a + 1
+        print(a)
